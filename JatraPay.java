da89c1c259be208a4a4df35b6d0ba4f1dcc24666
Fahmida Sultana <fahmidasultana.info@gmail.com>
2025-11-28 19:49:42 +0600

    Update JatraPay.java

--- JatraPay.java
+++ JatraPay.java
@@ -3,8 +3,8 @@ import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-// model
-// c1
+
+
 class TransportService {
     private String route;
     private double fare;
@@ -22,8 +22,6 @@ class TransportService {
         return fare;
     }
 }
-
-// c2
 class PremiumService extends TransportService {
     private double premiumCharge;
     public PremiumService(String route, double base, double charge) {
@@ -37,8 +35,6 @@ class PremiumService extends TransportService {
         return premiumCharge;
     }
 }
-
-// c3
 class Passenger {
     private String name;
     private String type;
@@ -53,8 +49,6 @@ class Passenger {
         return type;
     }
 }
-
-// c4
 class Payment {
     private String method;
     public Payment(String method) {
@@ -68,8 +62,6 @@ class Payment {
     }
 }
 
-// controller
-// c5
 class TransactionController {
     public String processTransaction(Passenger passenger, TransportService service, Payment payment,
             double fareEntered) {
@@ -78,8 +70,7 @@ class TransactionController {
     }
 }
 
-// view
-// c6
+
 public class JatraPay {
     private JFrame frame;
     private JComboBox<String> startCombo;
@@ -89,6 +80,9 @@ public class JatraPay {
     private JRadioButton qrRadio, ussdRadio;
     private JButton processButton;
     private TransactionController controller;
+
+
+    private JTextArea historyArea;
 
     private final String[] LOCATIONS = {
             "GEC", "Bahaddarhat", "Muradpur", "Oxygen", "Chawkbazar", "Station Road"
@@ -102,14 +96,13 @@ public class JatraPay {
     private void initialize() {
 
         frame = new JFrame("JatraPay - makes your journey smooth");
-        frame.setSize(500, 500);
+        frame.setSize(900, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridBagLayout()); 
+        frame.setLayout(new GridBagLayout());
 
+       
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        leftPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        
         leftPanel.setBorder(
             BorderFactory.createCompoundBorder(
                 BorderFactory.createTitledBorder("JatraPay Transaction Details"),
@@ -118,73 +111,111 @@ public class JatraPay {
         );
         leftPanel.setBackground(Color.WHITE);
 
-        
-        // start
+
         leftPanel.add(new JLabel("Enter Starting Location:"));
         startCombo = new JComboBox<>(LOCATIONS);
         startCombo.setEditable(true);
-        startCombo.setMaximumSize(new Dimension(700, 50)); 
-        startCombo.setAlignmentX(Component.LEFT_ALIGNMENT); 
-        
+        startCombo.setMaximumSize(new Dimension(700, 50));
+        startCombo.setAlignmentX(Component.LEFT_ALIGNMENT);
         leftPanel.add(startCombo);
 
-        // destination
+      
         leftPanel.add(Box.createVerticalStrut(10));
         leftPanel.add(new JLabel("Enter Destination:"));
         destCombo = new JComboBox<>(LOCATIONS);
         destCombo.setEditable(true);
-        destCombo.setMaximumSize(new Dimension(700, 50)); 
-        destCombo.setAlignmentX(Component.LEFT_ALIGNMENT);
-        
+        destCombo.setMaximumSize(new Dimension(700, 50));
+        destCombo.setAlignmentX(Component.LEFT_ALIGNMENT); 
         leftPanel.add(destCombo);
 
-        // fare
+     
         leftPanel.add(Box.createVerticalStrut(10));
         leftPanel.add(new JLabel("Enter Fare Amount (1-100TK):"));
         fareField = new JTextField();
-        fareField.setMaximumSize(new Dimension(700, 70)); 
-        fareField.setAlignmentX(Component.LEFT_ALIGNMENT); 
-        
+        fareField.setMaximumSize(new Dimension(700, 70));
+        fareField.setAlignmentX(Component.LEFT_ALIGNMENT);
         leftPanel.add(fareField);
 
-        //pay method
         leftPanel.add(Box.createVerticalStrut(10));
         leftPanel.add(new JLabel("Payment Method:"));
 
         qrRadio = new JRadioButton("QR");
         qrRadio.setSelected(true);
-        qrRadio.setBackground(Color.WHITE); 
-        
+        qrRadio.setBackground(Color.WHITE);
+
         ussdRadio = new JRadioButton("USSD");
         ussdRadio.setBackground(Color.WHITE);
 
         ButtonGroup bg = new ButtonGroup();
         bg.add(qrRadio);
         bg.add(ussdRadio);
-
-        JPanel radioPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 0));
+        
+        
+        JPanel radioPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 0)); 
         radioPanel.setBackground(Color.WHITE);
         radioPanel.add(qrRadio);
         radioPanel.add(ussdRadio);
-        radioPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        
+       
+        radioPanel.setAlignmentX(Component.LEFT_ALIGNMENT); 
+       
+        radioPanel.setMaximumSize(new Dimension(700, radioPanel.getPreferredSize().height)); 
+        
         leftPanel.add(radioPanel);
 
         processButton = new JButton("Process");
-        processButton.setMaximumSize(new Dimension(700, 50)); 
-        processButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        processButton.setMaximumSize(new Dimension(700, 50));
+        processButton.setAlignmentX(Component.LEFT_ALIGNMENT); 
         
         leftPanel.add(Box.createVerticalStrut(20));
         leftPanel.add(processButton);
         leftPanel.add(Box.createVerticalStrut(20));
 
-        frame.add(leftPanel, new GridBagConstraints());
-        frame.getContentPane().setBackground(new Color(240, 240, 240));
 
-        // Action
+       
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+        rightPanel.setBorder(
+            BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder("Transaction History"),
+                BorderFactory.createEmptyBorder(10, 10, 10, 10)
+            )
+        );
+
+        historyArea = new JTextArea();
+        historyArea.setEditable(false);
+        historyArea.setFont(new Font("Monospaced", Font.PLAIN, 13));
+
+        JScrollPane scroll = new JScrollPane(historyArea);
+        rightPanel.add(scroll, BorderLayout.CENTER);
+
+
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        frame.add(leftPanel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        frame.add(rightPanel, gbc);
+
+
+       
         processButton.addActionListener(e -> onProcess());
 
-        frame.setLocationRelativeTo(null); 
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    
+    private void addHistory(String text) {
+       
+        historyArea.append(text + "\n-----------------------------------\n");
     }
 
     private void onProcess() {
@@ -205,7 +236,7 @@ public class JatraPay {
             return;
         }
 
-        // fare check
+   
         String fareText = fareField.getText().trim();
         double fareValue;
 
@@ -225,7 +256,7 @@ public class JatraPay {
         Payment payment = new Payment(paymentMethod);
         String status = controller.processTransaction(passenger, service, payment, fareValue);
 
-        // receipt
+       
         String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
 
         String receipt = "Date & Time: " + now + "\n\n" +
@@ -234,7 +265,13 @@ public class JatraPay {
                 "Payment Method: " + paymentMethod + "\n" +
                 "Status: " + status;
 
+    
         JOptionPane.showMessageDialog(frame, receipt, "✅ Payment Receipt", JOptionPane.INFORMATION_MESSAGE);
+
+        
+        addHistory(receipt);
+
+       
         startCombo.setSelectedItem("");
         destCombo.setSelectedItem("");
         fareField.setText("");
